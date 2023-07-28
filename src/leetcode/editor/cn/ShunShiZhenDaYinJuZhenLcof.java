8110bc5723819b4a5b51faaecb4943eb56ca4178
zhaowenqiang1 <zhaowenqiang1@xiaomi.com>
2023-07-28 14:45:20 +0800

    最长回文串 顺时针打印矩阵

--- src/leetcode/editor/cn/ShunShiZhenDaYinJuZhenLcof.java
+++ src/leetcode/editor/cn/ShunShiZhenDaYinJuZhenLcof.java
@@ -40,46 +40,81 @@ public class ShunShiZhenDaYinJuZhenLcof{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] spiralOrder(int[][] matrix) {
-        int m = matrix.length;
-        if(m==0) return  new int[]{};
-        int n = matrix[0].length;
-        int upper_bund = 0, lower_bound = m - 1;
-        int left_bound = 0, right_bound = n - 1;
-        List<Integer> res = new ArrayList<Integer>();
+//        int m = matrix.length;
+//        if(m==0) return  new int[]{};
+//        int n = matrix[0].length;
+//        int upper_bund = 0, lower_bound = m - 1;
+//        int left_bound = 0, right_bound = n - 1;
+//        List<Integer> res = new ArrayList<Integer>();
+//
+//        while (res.size() < n * m) {
+//            if (upper_bund <= lower_bound) {
+//                for (int i = left_bound; i <= right_bound; i++) {
+//                    res.add((matrix[upper_bund][i]));
+//                }
+//                upper_bund++;
+//            }
+//            if (left_bound <= right_bound) {
+//                for (int i = upper_bund; i <= lower_bound; i++) {
+//                    res.add(matrix[i][right_bound]);
+//                }
+//                right_bound--;
+//            }
+//            if (upper_bund <= lower_bound) {
+//                for (int i = right_bound; i >= left_bound; i--) {
+//                    res.add((matrix[lower_bound][i]));
+//                }
+//                lower_bound--;
+//            }
+//            if (left_bound <= right_bound) {
+//                for (int i = lower_bound; i >= upper_bund; i--) {
+//                    res.add(matrix[i][left_bound]);
+//                }
+//                left_bound++;
+//            }
+//
+//        }
+//        int[] res2 = new int[n * m];
+//        for (int i = 0; i < n * m; i++) {
+//            res2[i] = res.get(i);
+//        }
+//        return res2;
+        int m=matrix.length;;
+        if(m==0) return new int[]{};
+        int n=matrix[0].length;
+        int upperBond=0,lowerBond=m-1;
+        int leftBond=0,rightBond=n-1;
 
-        while (res.size() < n * m) {
-            if (upper_bund <= lower_bound) {
-                for (int i = left_bound; i <= right_bound; i++) {
-                    res.add((matrix[upper_bund][i]));
+        List<Integer> res=new ArrayList<>();
+        while(res.size()<n*m){
+            if(upperBond<=lowerBond){
+                for(int i=leftBond;i<=rightBond;i++){
+                    res.add(matrix[upperBond][i]);
                 }
-                upper_bund++;
+                upperBond++;
             }
-            if (left_bound <= right_bound) {
-                for (int i = upper_bund; i <= lower_bound; i++) {
-                    res.add(matrix[i][right_bound]);
+            if(leftBond<=rightBond){
+                for(int i=upperBond;i<=lowerBond;i++){
+                    res.add(matrix[i][rightBond]);
                 }
-                right_bound--;
+                rightBond--;
             }
-            if (upper_bund <= lower_bound) {
-                for (int i = right_bound; i >= left_bound; i--) {
-                    res.add((matrix[lower_bound][i]));
+            if(upperBond<=lowerBond){
+                for(int i=rightBond;i>=leftBond;i--){
+                    res.add(matrix[lowerBond][i]);
                 }
-                lower_bound--;
+                lowerBond--;
             }
-            if (left_bound <= right_bound) {
-                for (int i = lower_bound; i >= upper_bund; i--) {
-                    res.add(matrix[i][left_bound]);
+            if(leftBond<=rightBond){
+                for(int i=lowerBond;i>=upperBond;i--){
+                    res.add(matrix[i][leftBond]);
                 }
-                left_bound++;
+                leftBond++;
             }
-
 
         }
-        int[] res2 = new int[n * m];
-        for (int i = 0; i < n * m; i++) {
-            res2[i] = res.get(i);
-        }
-        return res2;
+        return res.stream().mapToInt(Integer::intValue).toArray();
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
