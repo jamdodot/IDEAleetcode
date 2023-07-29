c0c3b55fb68e2b7698ebd5dfa483e96787a745b9
zwq <jamdodot@gmail.com>
2023-07-29 20:33:23 +0800

    二叉树的层平均

---
+++ src/leetcode/editor/cn/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,114 @@
+package leetcode.editor.cn;
+
+//给定一个非空二叉树的根节点
+// root , 以数组的形式返回每一层节点的平均值。与实际答案相差 10⁻⁵ 以内的答案可以被接受。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[3.00000,14.50000,11.00000]
+//解释：第 0 层的平均值为 3,第 1 层的平均值为 14.5,第 2 层的平均值为 11 。
+//因此返回 [3, 14.5, 11] 。
+// 
+//
+// 示例 2: 
+//
+// 
+//
+// 
+//输入：root = [3,9,20,15,7]
+//输出：[3.00000,14.50000,11.00000]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 
+//
+// 
+// 树中节点数量在 [1, 10⁴] 范围内 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 424 👎 0
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class AverageOfLevelsInBinaryTree{
+    public static void main(String[] args) {
+        Solution solution = new AverageOfLevelsInBinaryTree().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        Deque<TreeNode> deque=new ArrayDeque<>();
+        List<Double> res=new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        deque.add(root);
+        while(!deque.isEmpty()){
+            List<Integer> temp=new ArrayList<>();
+            int size=deque.size();
+            double levelSum=0.0;
+            int levelSize=deque.size();
+            for(;size>0;size--){
+                TreeNode pop = deque.pop();
+                temp.add(pop.val);
+                levelSum+=pop.val;
+                if(pop.left!=null){
+                    deque.add(pop.left);
+                }
+                if(pop.right!=null){
+                    deque.add(pop.right);
+                }
+
+            }
+            res.add(levelSum/levelSize);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
