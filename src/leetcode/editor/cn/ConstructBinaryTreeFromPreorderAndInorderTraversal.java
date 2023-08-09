88d7d91dd070786fbe267f346ee56d4f0ec86266
zhaowenqiang1 <zhaowenqiang1@xiaomi.com>
2023-08-09 16:46:26 +0800

    中序先序构建二叉树理解

---
+++ src/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,97 @@
+package leetcode.editor.cn;
+
+//给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并
+//返回其根节点。 
+//
+// 
+//
+// 示例 1: 
+// 
+// 
+//输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//输出: [3,9,20,null,null,15,7]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: preorder = [-1], inorder = [-1]
+//输出: [-1]
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= preorder.length <= 3000 
+// inorder.length == preorder.length 
+// -3000 <= preorder[i], inorder[i] <= 3000 
+// preorder 和 inorder 均 无重复 元素 
+// inorder 均出现在 preorder 
+// preorder 保证 为二叉树的前序遍历序列 
+// inorder 保证 为二叉树的中序遍历序列 
+// 
+//
+// Related Topics 树 数组 哈希表 分治 二叉树 👍 2034 👎 0
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal{
+    public static void main(String[] args) {
+        Solution solution = new ConstructBinaryTreeFromPreorderAndInorderTraversal().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+class Solution {
+    Map<Integer,Integer> map=new HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for(int i=0;i<inorder.length;i++){
+            map.put(inorder[i],i);
+        }
+        TreeNode node = traverse(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+        return node;
+
+    }
+    public TreeNode traverse(int[] preorder, int[] inorder, int preLeft, int preRight, int inLeft, int inRight){
+        if(preLeft>preRight||inLeft>inRight) return null;
+
+        Integer mid = map.get(preorder[preLeft]);
+        TreeNode node = new TreeNode(inorder[mid]);
+        node.left=traverse(preorder, inorder, preLeft+1, mid-1-inLeft+1+preLeft, inLeft, mid-1);
+        node.right=traverse(preorder, inorder,mid-1-inLeft+1+preLeft+1,inRight,mid+1,inRight);
+        return node;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
