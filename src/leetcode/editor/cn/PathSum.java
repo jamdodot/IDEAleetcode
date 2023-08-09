b7d604dcca3f50d00652b094eeb410dd3847b4db
zhaowenqiang1 <zhaowenqiang1@xiaomi.com>
2023-08-09 19:43:23 +0800

    二叉树路径和

---
+++ src/leetcode/editor/cn/PathSum.java
@@ -0,0 +1,111 @@
+package leetcode.editor.cn;
+
+//给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和
+// targetSum 。如果存在，返回 true ；否则，返回 false 。 
+//
+// 叶子节点 是指没有子节点的节点。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+//输出：true
+//解释：等于目标和的根节点到叶节点路径如上图所示。
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [1,2,3], targetSum = 5
+//输出：false
+//解释：树中存在两条根节点到叶子节点的路径：
+//(1 --> 2): 和为 3
+//(1 --> 3): 和为 4
+//不存在 sum = 5 的根节点到叶子节点的路径。 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [], targetSum = 0
+//输出：false
+//解释：由于树是空的，所以不存在根节点到叶子节点的路径。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数目在范围 [0, 5000] 内 
+// -1000 <= Node.val <= 1000 
+// -1000 <= targetSum <= 1000 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1223 👎 0
+
+public class PathSum{
+    public static void main(String[] args) {
+        Solution solution = new PathSum().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+class Solution {
+    int target=0;
+    boolean res=false;
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        this.target=targetSum;
+        if(root==null) return res;
+        dfs(root, 0);
+        return res;
+    }
+    public void dfs(TreeNode root, int sum){
+        if(root.left==null&&root.right==null){
+            sum+=root.val;
+            if(sum==target){
+                res=true;
+            }
+            return;
+        }
+        if(root.left!=null){
+            dfs(root.left,sum+root.val);
+        }
+        if(root.right!=null){
+            dfs(root.right,sum+root.val);
+        }
+
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
