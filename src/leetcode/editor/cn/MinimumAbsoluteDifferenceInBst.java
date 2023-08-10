bae4401e1fa6de1be52da1ce648ff2fc59ef761d
zhaowenqiang1 <zhaowenqiang1@xiaomi.com>
2023-08-10 19:41:27 +0800

    验证二叉搜索树 二叉搜索数的最小绝对差值

---
+++ src/leetcode/editor/cn/MinimumAbsoluteDifferenceInBst.java
@@ -0,0 +1,92 @@
+package leetcode.editor.cn;
+
+//给你一个二叉搜索树的根节点 root ，返回 树中任意两不同节点值之间的最小差值 。 
+//
+// 差值是一个正数，其数值等于两值之差的绝对值。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [4,2,6,1,3]
+//输出：1
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [1,0,48,null,null,12,49]
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数目范围是 [2, 10⁴] 
+// 0 <= Node.val <= 10⁵ 
+// 
+//
+// 
+//
+// 注意：本题与 783 https://leetcode-cn.com/problems/minimum-distance-between-bst-
+//nodes/ 相同 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉搜索树 二叉树 👍 486 👎 0
+
+public class MinimumAbsoluteDifferenceInBst{
+    public static void main(String[] args) {
+        Solution solution = new MinimumAbsoluteDifferenceInBst().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+class Solution {
+    int res=Integer.MAX_VALUE;
+    TreeNode last=null;
+    public int getMinimumDifference(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+    public void dfs(TreeNode root){
+        if(root==null) return;
+        dfs(root.left);
+        if(last!=null){
+            res=Math.min(res,Math.abs(root.val-last.val));
+        }
+        last=root;
+        dfs(root.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/leetcode/editor/cn/ValidateBinarySearchTree.java
@@ -0,0 +1,100 @@
+package leetcode.editor.cn;
+
+//给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。 
+//
+// 有效 二叉搜索树定义如下： 
+//
+// 
+// 节点的左子树只包含 小于 当前节点的数。 
+// 节点的右子树只包含 大于 当前节点的数。 
+// 所有左子树和右子树自身必须也是二叉搜索树。 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [2,1,3]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [5,1,4,null,null,3,6]
+//输出：false
+//解释：根节点的值是 5 ，但是右子节点的值是 4 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在[1, 10⁴] 内 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 2117 👎 0
+
+public class ValidateBinarySearchTree{
+    public static void main(String[] args) {
+        Solution solution = new ValidateBinarySearchTree().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+class Solution {
+    boolean res=true;
+    long max=Long.MAX_VALUE;
+    long min=Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+
+        dfs(root, max, min);
+        return res;
+
+    }
+    public void dfs(TreeNode root,long max, long min){
+        if(root.val>=max||root.val<=min) res=false;
+        if(root==null) return;
+        if(root.left!=null){
+          dfs(root.left, root.val, min);
+        }
+        if(root.right!=null){
+            dfs(root.right, max, root.val);
+        }
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
