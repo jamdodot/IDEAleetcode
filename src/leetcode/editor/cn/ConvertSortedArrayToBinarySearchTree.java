08249112c4bd17c81956e85983eedcb41e753eda
zwq <jamdodot@gmail.com>
2023-08-13 11:35:52 +0800

    有序数组转换为二叉搜索树

--- src/leetcode/editor/cn/ConvertSortedArrayToBinarySearchTree.java
+++ src/leetcode/editor/cn/ConvertSortedArrayToBinarySearchTree.java
@@ -57,9 +57,29 @@ public class ConvertSortedArrayToBinarySearchTree{
  *     }
  * }
  */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
-
+        return buildTree(nums, 0, nums.length-1);
+    }
+    public TreeNode buildTree(int[] nums, int left, int right){
+        if(left>right) return null;
+        int mid=(right+left+1)/2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = buildTree(nums, left, mid-1);
+        root.right = buildTree(nums, mid+1, right);
+        return root;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
